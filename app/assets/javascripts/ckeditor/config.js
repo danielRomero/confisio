8dc669ddd41ba42ab5d0762626c1da8cbed0a198
Daniel Romero <daniel.romero.esteban@gmail.com>
2015-06-03 22:10:40 +0200

    cookies message

--- app/assets/javascripts/ckeditor/config.js
+++ app/assets/javascripts/ckeditor/config.js
@@ -1,9 +1,10 @@
 CKEDITOR.editorConfig = function (config) {
   config.extraPlugins = 'youtube';
   config.toolbar_mini = [
-    ["Bold",  "Italic",  "Underline",  "Strike",  "-",  "Subscript",  "Superscript"],
+    [ 'Bold',  'Italic',  'Underline',  'Strike',  '-',  'Subscript',  'Superscript'],
     [ 'Cut', 'Copy', 'Paste', 'PasteText', 'PasteFromWord', '-', 'Undo', 'Redo' ],
-    [ 'Image', 'Youtube', 'Table', 'HorizontalRule','-', 'Smiley', 'SpecialChar', 'PageBreak', 'Iframe' ],
+    [ 'Image', 'Youtube', 'Table', 'HorizontalRule','-', 'Smiley', 'SpecialChar', 'PageBreak' ],
+    [ 'Link', 'Unlink' ],
     [ 'Styles', 'Format', 'Font', 'FontSize' ],
     [ 'TextColor', '-', 'BGColor' ]
   ];
