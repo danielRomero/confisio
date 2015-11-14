ff5426c31050bc5dc0d3f3abeea3f03892417bbf
Daniel Romero <daniel.romero.esteban@gmail.com>
2015-11-14 19:06:13 +0100

    error 404, 500

--- app/assets/javascripts/ckeditor/config.js
+++ app/assets/javascripts/ckeditor/config.js
@@ -1,9 +1,8 @@
 CKEDITOR.editorConfig = function (config) {
-  config.extraPlugins = 'youtube';
   config.toolbar_mini = [
     [ 'Bold',  'Italic',  'Underline',  'Strike',  '-',  'Subscript',  'Superscript'],
     [ 'Cut', 'Copy', 'Paste', 'PasteText', 'PasteFromWord', '-', 'Undo', 'Redo' ],
-    [ 'Image', 'Youtube', 'Table', 'HorizontalRule', '-', 'Smiley', 'SpecialChar', 'PageBreak' ],
+    [ 'Image', 'Table', 'HorizontalRule', '-', 'Smiley', 'SpecialChar', 'PageBreak' ],
     [ 'Link', '-', 'Unlink' ],
     ['JustifyLeft', 'JustifyCenter', '-', 'JustifyRight', 'JustifyBlock'],
     [ 'Styles', 'Format', 'Font', 'FontSize' ],
