c3a01bd811d2de1256fc1e2d3994a250d8841584
Daniel Romero <daniel.romero.esteban@gmail.com>
2016-03-04 16:13:51 +0100

    ++

--- config/deploy.rb
+++ config/deploy.rb
@@ -30,9 +30,9 @@ set :normalize_asset_timestamps, %{public/images public/javascripts public/style
 
 # Default value for :linked_files is []
 #set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml', '.env')
-set :linked_files, %w{config/database.yml config/secrets.yml .env}
+set :linked_files, %w{config/database.yml config/secrets.yml .env .ruby-gemset .ruby-version}
 
-set(:config_files, %w(config/database.yml config/secrets.yml .env))
+set(:config_files, %w(config/database.yml config/secrets.yml .env .ruby-gemset .ruby-version))
 
 # Default value for linked_dirs is []
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
@@ -40,6 +40,7 @@ set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', '
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
 set :default_env, { rvm_bin_path: '~/.rvm/bin' }
+set :rvm_ruby_version, '2.3.0'
 
 # Default value for keep_releases is 5
 # set :keep_releases, 5
@@ -50,7 +51,5 @@ set :whenever_identifier, ->{ "#{fetch(:application)}_#{fetch(:stage)}" }
 
 # set the locations that we will look for changed assets to determine whether to precompile
 set :assets_dependencies, %w(app/assets/ lib/assets vendor/assets Gemfile config/routes.rb)
-
-set :rvm_ruby_version, '2.3.0'
 
 class PrecompileRequired < StandardError; end
